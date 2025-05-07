b278523d7b067dee0418eaef0adfad2a20448ae5
Ali Samir <ali.samir@taager.com>
2025-05-07 20:48:46 +0300

    chore: update dependencies and remove unused SVG files
    - Added several Radix UI components and utility libraries to dependencies.
    - Updated existing dependencies including TypeScript and Tailwind CSS.
    - Removed unused SVG files from the public directory to clean up the project.

---
+++ components/Logo.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+
+const Logo = ({
+  className,
+  spanDesign,
+}: {
+  className?: string;
+  spanDesign?: string;
+}) => {
+  return (
+    <Link href="/" className="inline-flex">
+      <h2
+        className={cn(
+          "text-2xl text-shop_dark_green font-black tracking-wider uppercase hover:text-shop_light_green hoverEffect group font-sans",
+          className
+        )}
+      >
+        Shopi
+        <span
+          className={cn(
+            "text-shop_light_green group-hover:text-shop_dark_green hoverEffect",
+            spanDesign
+          )}
+        >
+          Cart
+        </span>
+      </h2>
+    </Link>
+  );
+};
+
+export default Logo;
